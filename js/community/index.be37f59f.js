ec1d57c6a7ecdf225b1a7d18da4d789c6a614bfc
qingshui <1713024161@qq.com>
2023-12-08 11:12:36 +0800

    1

--- js/community/index.be37f59f.js
+++
@@ -1,1 +0,0 @@
-import{d as a,S as e,X as s}from"../@vue/@vue.54b7059a.js";const t=a({__name:"index",setup:a=>(a,t)=>(e(),s("div",null," 个人中心 "))});export{t as default};
--- js/guestbook/index-legacy.19a7479e.js
+++
@@ -1,1 +0,0 @@
-System.register(["../@vue-legacy/@vue.258468b2.js"],(function(e,t){"use strict";var u,n,s;return{setters:[e=>{u=e.d,n=e.S,s=e.X}],execute:function(){e("default",u({__name:"index",setup:e=>(e,t)=>(n(),s("div",null," 留言板 "))}))}}}));
